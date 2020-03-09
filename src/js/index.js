be8ef00eddf51305aff285a2dd4db7e282c9ba25
Nelson Ramirez <melsin17@yahoo.com>
2020-03-09 02:24:51 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,53 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  const suite = ["♠", "♣", "♥", "♦"];
+
+  const number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const selectRandomIndex = array =>
+    array[Math.floor(Math.random() * (array.length - 1 - 0) + 0)];
+
+  const suiteToClass = suite => {
+    switch (suite) {
+      case "♠":
+        return "spades";
+      case "♣":
+        return "clubs";
+      case "♥":
+        return "hearts";
+      case "♦":
+        return "diamonds";
+      default:
+        return "";
+    }
+  };
+
+  const suiteEl = Array.from(document.querySelectorAll(".suite"));
+  const numberEl = document.querySelector(".number");
+
+  const suitevalue = selectRandomIndex(suite);
+  const numbervalue = selectRandomIndex(number);
+
+  console.log(suitevalue, numbervalue);
+  console.log(suiteEl);
+  suiteEl.forEach(el => {
+    el.classList.add(suiteToClass(suitevalue));
+    el.textContent = suitevalue;
+  });
+  numberEl.textContent = numbervalue;
 };
